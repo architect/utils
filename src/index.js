e61276887fce3775775ce3133cf4b267f5030533
Brian LeRoux <b@brian.io>
2019-05-29 10:45:55 -0700

    migrate in populateEnv

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
+let getLambdaName = require('./get-lambda-name')
+let populateEnv = require('./populate-env')
 let portInUse = require('./port-in-use')
 let readArc = require('./read-arc')
+let toLogicalID = require('./to-logical-id')
 //let init = 
 //let inventory = 
-let toLogicalID = require('./to-logical-id')
-let getLambdaName = require('./get-lambda-name')
 
 module.exports = {
+  getLambdaName,
+  populateEnv,
   portInUse,
   readArc,
   toLogicalID,
-  getLambdaName,
 }
