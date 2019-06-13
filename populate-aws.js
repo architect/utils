3f1d3258d29ab1f7e8dde0caf66b56ba8f7f5ca5
Ryan Block <rblock@gmail.com>
2019-06-13 13:24:54 -0700

    Only attempt loading AWS env vars if successfully loaded arc; tidies up property finder

--- populate-aws.js
+++ populate-aws.js
@@ -8,19 +8,25 @@ module.exports = function populateAWS () {
   try {
     let parsed = readArcFile()
     arc = parsed.arc
+
+    // Attempt to load and populate AWS_REGION + AWS_PROFILE
+    if (arc && arc.aws) {
+      let region = arc.aws.find(e=> e[0] === 'region')
+      let profile = arc.aws.find(e=> e[0] === 'profile')
+
+      if (region) {
+        process.env.AWS_REGION = region[1]
+      }
+
+      if (profile) {
+        process.env.AWS_PROFILE = profile[1]
+      }
+    }
   }
   catch(e) {
     if (e.message != 'not_found') {
       // Don't exit process here even if .arc isn't found; caller should be responsible (via ./read-arc)
       console.log(e)
     }
-  }
-
-  // Attempt to load and populate AWS_REGION + AWS_PROFILE
-  if (arc.aws && arc.aws.findIndex(e => e[0] === 'region') !== -1) {
-    process.env.AWS_REGION = arc.aws[arc.aws.findIndex(e => e[0] === 'region')][1]
-  }
-  if (arc.aws && arc.aws.findIndex(e => e[0] === 'profile') !== -1) {
-    process.env.AWS_PROFILE = arc.aws[arc.aws.findIndex(e => e[0] === 'profile')][1]
   }
 }
