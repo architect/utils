dc2ce3c8d62dd7be18552f0e96fa08b79a792ef2
filmaj <maj.fil@gmail.com>
2019-07-08 14:05:47 -0500

    porting in validate and its helper functions from architect/architect into utils module.

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 let banner = require('./banner')
 let getLambdaName = require('./get-lambda-name')
+let getLayers = require('./get-layers')
+let getRuntime = require('./get-runtime')
 let init = require('./init')
 let inventory = require('./inventory')
 let populateEnv = require('./populate-env')
 let portInUse = require('./port-in-use')
 let readArc = require('./read-arc')
 let toLogicalID = require('./to-logical-id')
+let validate = require('./validate')
 
 module.exports = {
   banner,
   getLambdaName,
+  getLayers,
+  getRuntime,
   init,
   inventory,
   populateEnv,
   portInUse,
   readArc,
   toLogicalID,
+  validate,
 }
