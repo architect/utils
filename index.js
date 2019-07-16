06a90e2d7bf8eeb190db52245186a0737e65f7f6
Ryan Block <rblock@gmail.com>
2019-07-15 17:26:56 -0700

    Add fingerprint to entry and comment utils list

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let banner = require('./banner')
+let fingerprint = require('./fingerprint')
 let getLambdaName = require('./get-lambda-name')
 let getLayers = require('./get-layers')
 let getRuntime = require('./get-runtime')
@@ -11,15 +12,16 @@ let toLogicalID = require('./to-logical-id')
 let validate = require('./validate')
 
 module.exports = {
-  banner,
-  getLambdaName,
-  getLayers,
-  getRuntime,
-  init,
-  inventory,
-  populateEnv,
-  portInUse,
-  readArc,
-  toLogicalID,
-  validate,
+  banner,         // Prints banner and loads basic env vars and AWS creds
+  fingerprint,    // Generates public/static.json for @static fingerprint true
+  getLambdaName,  // Get Lambda name from Arc path
+  getLayers,      // Get layer config from Arc file or config
+  getRuntime,     // Get runtime config from Arc file or config
+  init,           // Boilerplate code generator for Arc projects
+  inventory,      // Get inventory of current AWS resources from Arc file
+  populateEnv,    // Populate env vars from .arc-env config
+  portInUse,      // Checks to see if a port is in use
+  readArc,        // Reads Arc file and returns raw + parsed versions
+  toLogicalID,    // Converts dash casing into Pascal casing for
+  validate,       // Validates an Arc file
 }
