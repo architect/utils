9960c15cef85672ee0494b727c6a6a21722801ec
kj <kristoferjoseph@gmail.com>
2019-08-28 15:02:35 -0700

    Updates default http template.

--- init/public-code.js
+++ init/public-code.js
@@ -8,6 +8,7 @@ let html = `
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Architect</title>
   <link rel=stylesheet type=text/css href="/index.css">
 </head>
