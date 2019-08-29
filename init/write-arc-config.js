74a139c901d0a2ccca4ab9d54c7045e9c37c190b
Ryan Block <rblock@gmail.com>
2019-08-28 20:37:46 -0700

    Use same order for .arc-config as in docs

--- init/write-arc-config.js
+++ init/write-arc-config.js
@@ -3,9 +3,9 @@ let fs = require('fs')
 module.exports = function writeArcConfig({configPath, runtime}, callback) {
   let config = `@aws
 runtime ${runtime}
+# memory 1152
 # timeout 30
 # concurrency 1
-# memory 1152
 `
   fs.writeFile(configPath, config, callback)
 }
