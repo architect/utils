ba152bc83c86579db815395d19e05d62517a1c72
Ryan Block <rblock@gmail.com>
2019-09-07 18:13:00 -0700

    Tidy up logic, return values to enable capturing status

--- updater/index.js
+++ updater/index.js
@@ -11,50 +11,60 @@ let {
 
 /**
  * Updater
- * - `status` - prints an affirmative status update - `chars.done`
+ * - `status` - prints an affirmative status update - `chars.start`
  *   - optional: supporting info on new lines with additional params
+ *   - aliases: `update`
  * - `start`  - start progress indicator - `spinner`||`chars.start`
  * - `done`   - end progress indicator with an update - `chars.done`
+ *   - aliases: `stop`
  * - `cancel` - cancel progress indicator without update - !char
  * - `err`   - pretty print an error - `chars.err`
- *   - aliases to `error` and `fail`
+ *   - aliases: `error` and `fail`
+ *
+ * Each method should also return a value to enable capture of progress data
  */
 module.exports = function updater(name) {
   restore() // Restores cursor on unexpected exit
+  name = name ? chalk.grey(name) : 'Info'
+  let isCI = process.env.CI || !process.stdout.isTTY
   let running = false
-  let n = chalk.grey(name) || ''
-  let m = ''
   let {frames, timing} = spinner
 
-
-  function progressIndicator() {
-    let i = 0
+  function progressIndicator(info) {
     write(hideCursor)
     // End-user progress mode
-    if (!running && !process.env.CI) {
+    if (!running && !isCI) {
+      let i = 0
       running = setInterval(function() {
-        write(`${chalk.cyan(frames[i = ++i % frames.length])} ${n} ${m}`)
+        write(`${chalk.cyan(frames[i = ++i % frames.length])} ${info}`)
         reset()
       }, timing)
     }
     // CI mode: updates console with status messages but not animations
-    else if (!running && process.env.CI && m.length > 0) {
-      console.log(`${chars.start} ${n} ${m}`)
+    else if (!running && isCI && info.length > 0) {
+      console.log(`${chars.start} ${info}`)
     }
   }
 
-  function status(msg, ...info) {
-    msg = msg || ''
-    m = chalk.cyan(msg)
-    console.log(`${chars.start} ${n} ${m}`)
-    if (info)
-      info.forEach(i => console.log(chalk.dim(`  | ${i}`)))
+  function status(msg, ...more) {
+    msg = msg ? chalk.cyan(msg) : ''
+    let info = `${chars.start} ${name} ${msg}`.trim()
+    console.log(info)
+    if (more) {
+      more.forEach(i => {
+        let add = chalk.dim(`  | ${i}`)
+        console.log(add)
+        info += `\n${add}`
+      })
+    }
+    return info
   }
 
   function start(msg) {
-    msg = msg || ''
-    m = chalk.cyan(msg)
-    progressIndicator()
+    msg = msg ? chalk.cyan(msg) : ''
+    let info = `${name} ${msg}`.trim()
+    progressIndicator(info)
+    return `${chars.start} ${info}`
   }
 
   function done(newName, msg) {
@@ -63,32 +73,36 @@ module.exports = function updater(name) {
       newName = ''
     }
     if (newName) newName = chalk.grey(newName)
-    if (msg) m = chalk.cyan(msg)
+    if (msg) msg = chalk.cyan(msg)
+    cancel() // Maybe clear running status and reset
+    let info = `${chars.done} ${newName ? newName : name} ${msg ? msg : ''}`.trim()
+    console.log(info)
+    return info
+  }
+
+  function cancel() {
     if (running) {
       clearInterval(running)
       reset()
       clear()
       running = false // Prevent accidental second done print
     }
-    console.log(`${chars.done} ${newName ? newName : n} ${m}`)
-  }
-
-  function cancel() {
-    clearInterval(running)
-    running = false
-    write('\u001B[?25h')
   }
 
   function err(error) {
     if (running) cancel()
     if (error instanceof Error) error = error.message
-    console.log(`${chars.err} ${chalk.red('Error:')} ${error}`)
+    let info = `${chars.err} ${chalk.red('Error:')} ${error}`.trim()
+    console.log(info)
+    return info
   }
 
   return {
     start,
+    update: start,
     status,
     done,
+    stop: done,
     cancel,
     err,
     error: err,
