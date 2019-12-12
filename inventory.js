ccc581c6664b7562cb3ecb3af5447a364a908d02
Ryan Block <200964+ryanblock@users.noreply.github.com>
2019-12-12 15:44:00 -0800

    Legacy WS support (#40)
    * Adds support for legacy src/ws/ws-* paths
    
    * Additional backwards compat for WebSocket inventory

--- inventory.js
+++ inventory.js
@@ -101,27 +101,31 @@ module.exports = function inventory(arc) {
   }
 
   if (arc.ws) {
+    // Handle 5 vs 6 WS naming changes
+    let wsName = name => process.env.DEPRECATED ? `ws-${name}` : name
+    let dir = name => path.join('src', 'ws', wsName(name))
+
     report.websocketapis = [
-      `${arc.app}-ws-staging`,
-      `${arc.app}-ws-production`,
+      `${app}-ws-staging`,
+      `${app}-ws-production`,
     ]
     report.lambdas = report.lambdas.concat([
-      `${arc.app}-staging-ws-default`,
-      `${arc.app}-staging-ws-connect`,
-      `${arc.app}-staging-ws-disconnect`,
-      `${arc.app}-production-ws-default`,
-      `${arc.app}-production-ws-connect`,
-      `${arc.app}-production-ws-disconnect`,
+      `${app}-staging-ws-default`,
+      `${app}-staging-ws-connect`,
+      `${app}-staging-ws-disconnect`,
+      `${app}-production-ws-default`,
+      `${app}-production-ws-connect`,
+      `${app}-production-ws-disconnect`,
     ])
     report.types.ws = [
-      'ws-default',
-      'ws-connect',
-      'ws-disconnect',
+      wsName('default'),
+      wsName('connect'),
+      wsName('disconnect'),
     ]
     report.localPaths = report.localPaths.concat([
-      path.join('src', 'ws', 'default'),
-      path.join('src', 'ws', 'connect'),
-      path.join('src', 'ws', 'disconnect'),
+      dir('default'),
+      dir('connect'),
+      dir('disconnect'),
     ])
   }
 
