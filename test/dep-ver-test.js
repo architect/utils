9198d213ba6cf819e0991927ead748b1eb7a1e46
Ryan Block <rblock@gmail.com>
2020-02-14 08:52:33 -0800

    Update npm config to save-exact; pin devDeps, update test

--- test/dep-ver-test.js
+++ test/dep-ver-test.js
@@ -2,17 +2,31 @@ let test = require('tape')
 let package = require('../package.json')
 
 let deps = Object.keys(package.dependencies)
+let devDeps = Object.keys(package.devDependencies)
+let valid = ver => {
+  return !ver.startsWith('~') &&
+         !ver.startsWith('^') &&
+         !ver.toLowerCase().includes('x') &&
+         !ver.includes('*')
+}
 let ok = dep => `${dep} `.padEnd(25, '.')
 
-test('All primary dependencies must be version locked', t => {
+test('Primary dependencies must be version locked', t => {
   t.plan(deps.length)
   deps.forEach(dep => {
     let ver = package.dependencies[dep]
-    let valid = !ver.startsWith('~') &&
-                !ver.startsWith('^') &&
-                !ver.toLowerCase().includes('x') &&
-                !ver.includes('*')
-    if (valid)
+    if (valid(ver))
+      t.pass(`${ok(dep)} locked to v${ver}`)
+    else
+      t.fail(`${dep} must be version-locked in package.json: ${ver}`)
+  })
+})
+
+test('Dev dependencies must be version locked', t => {
+  t.plan(devDeps.length)
+  devDeps.forEach(dep => {
+    let ver = package.devDependencies[dep]
+    if (valid(ver))
       t.pass(`${ok(dep)} locked to v${ver}`)
     else
       t.fail(`${dep} must be version-locked in package.json: ${ver}`)
