9c19d9cc78acd3d11e506ae8dec4538cd6961978
filmaj <maj.fil@gmail.com>
2021-03-22 13:28:02 -0400

    test for backslash lambda name normalization

--- test/get-lambda-name/index-test.js
+++ test/get-lambda-name/index-test.js
@@ -2,7 +2,7 @@ let test = require('tape')
 let name = require('../../get-lambda-name')
 
 test('get-lambda-name converts routes to acceptable names', t => {
-  t.plan(7)
+  t.plan(8)
   t.equals(name('/'), '-index', 'root route returns -index')
   t.equals(name('/memories'), '-memories', 'named base route converts slashes')
   t.equals(name('/batman/robin'), '-batman-robin', 'intermediate slashes are converted to dashes')
@@ -10,4 +10,5 @@ test('get-lambda-name converts routes to acceptable names', t => {
   t.equals(name('/batman-robin'), '-batman_robin', 'dashes are converted to underscores')
   t.equals(name('/:batman'), '-000batman', 'colons are converted to triple zeros')
   t.equals(name('/path/*'), '-path-catchall', '* is converted to catchall')
+  t.equals(name('\\path\\*'), '-path-catchall', 'windows path slashes are converted appropriately')
 })
